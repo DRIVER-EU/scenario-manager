255032db765a39c0230c03cec266c96c3dd6cd5d
Erik Vullings <erik.vullings@gmail.com>
2019-03-04 11:04:21 +0100

    Added timeline component

--- packages/gui/src/components/session/session-state.ts
+++ packages/gui/src/components/session/session-state.ts
@@ -1,9 +1,14 @@
 import m, { FactoryComponent } from 'mithril';
 import { SocketSvc } from '../../services';
 import { IStateUpdate } from 'trial-manager-models';
+import { Timeline, ITimelineItem } from 'mithril-materialized';
+import { padLeft } from '../../utils';
 
 export const SessionState: FactoryComponent = () => {
-  const state = {
+  const timeFormatter = (d: Date) =>
+    `${padLeft(d.getUTCHours())}:${padLeft(d.getUTCMinutes())}:${padLeft(d.getUTCSeconds())}`;
+
+    const state = {
     socket: SocketSvc.socket,
     injectStates: {} as { [id: string]: IStateUpdate },
   };
@@ -14,6 +19,7 @@ export const SessionState: FactoryComponent = () => {
       socket.on('injectStates', (injectStates: { [id: string]: IStateUpdate }) => {
         state.injectStates = injectStates;
         console.table(injectStates);
+        m.redraw();
       });
     },
     onremove: () => {
@@ -22,7 +28,18 @@ export const SessionState: FactoryComponent = () => {
     },
     view: () => {
       const { injectStates } = state;
-      return m('pre', Object.keys(injectStates).map(k => injectStates[k]).join('<br>'));
+      return m(Timeline, {
+        timeFormatter,
+        items: Object.keys(injectStates)
+          .map(k => injectStates[k])
+          .sort((a, b) => a.lastTransitionAt > b.lastTransitionAt ? 1 : 0)
+          .map(s => ({
+            title: s.title,
+            datetime: new Date(s.lastTransitionAt),
+            iconName: 'ac_unit',
+            content: '',
+          } as ITimelineItem)),
+      });
     },
   };
 };
