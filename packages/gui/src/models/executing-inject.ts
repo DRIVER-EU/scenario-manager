c5c5aeb61dc7c47355411082d601e3944877883c
Erik Vullings <erik.vullings@gmail.com>
2019-03-05 16:00:25 +0100

    For managing injects that are running (VM)

---
+++ packages/gui/src/models/executing-inject.ts
@@ -0,0 +1,7 @@
+import { IInject, InjectState } from 'trial-manager-models';
+
+/** Inject that is actively running in a scenario, combining IInject and IStateUpdate information. */
+export interface IExecutingInject extends IInject {
+  state: InjectState;
+  lastTransitionAt: Date;
+}
