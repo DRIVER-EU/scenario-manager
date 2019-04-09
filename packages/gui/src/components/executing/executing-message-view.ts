fe43288deaebf5d0820570eb2e49af6a2b2b00da
Erik Vullings <erik.vullings@gmail.com>
2019-04-09 06:54:19 +0200

    Added more views for executing msg

--- packages/gui/src/components/executing/executing-message-view.ts
+++ packages/gui/src/components/executing/executing-message-view.ts
@@ -1,19 +1,22 @@
 import m, { FactoryComponent } from 'mithril';
-import { MessageType, InjectType } from 'trial-manager-models';
+import { MessageType, InjectType, InjectState } from 'trial-manager-models';
 import { IExecutingInject } from '../../models';
-import { RolePlayerMessageView } from '../messages';
+import { RolePlayerMessageView, PhaseMessageForm, GeoJsonMessageForm, OstChangeStageMessageForm } from '../messages';
+import { CapMessageForm } from '../messages/cap-message';
 
 export const ExecutingMessageView: FactoryComponent<{ inject?: IExecutingInject }> = () => {
   const getMessageForm = (inject: IExecutingInject) => {
     switch (inject.messageType) {
       case MessageType.ROLE_PLAYER_MESSAGE:
-        return m(RolePlayerMessageView, { inject });
-      // case MessageType.PHASE_MESSAGE:
-      //   return m(PhaseMessageForm, { inject });
-      // case MessageType.GEOJSON_MESSAGE:
-      //   return m(GeoJsonMessageForm, { inject });
-      // case MessageType.CHANGE_OBSERVER_QUESTIONNAIRES:
-      //   return m(OstChangeStageMessageForm, { inject });
+        return m(RolePlayerMessageView, { inject, disabled: inject.state === InjectState.EXECUTED });
+      case MessageType.CAP_MESSAGE:
+        return m(CapMessageForm, { inject, disabled: true });
+      case MessageType.PHASE_MESSAGE:
+        return m(PhaseMessageForm, { inject, disabled: true });
+      case MessageType.GEOJSON_MESSAGE:
+        return m(GeoJsonMessageForm, { inject, disabled: true });
+      case MessageType.CHANGE_OBSERVER_QUESTIONNAIRES:
+        return m(OstChangeStageMessageForm, { inject, disabled: true });
       default:
         return m('.row', 'TODO');
     }
