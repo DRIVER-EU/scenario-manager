803b70d49dc48d80343dc072578414c1935fe86e
Erik Vullings <erik.vullings@gmail.com>
2019-04-10 17:48:56 +0200

    Removed unused code

--- packages/server/src/utils/utils.ts
+++ packages/server/src/utils/utils.ts
@@ -1,21 +1,5 @@
 import { RunResult } from 'sqlite3';
 
-/**
- * Create a GUID
- * @see https://stackoverflow.com/a/2117523/319711
- *
- * @returns RFC4122 version 4 compliant GUID
- */
-export const uniqueId = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    // tslint:disable-next-line:no-bitwise
-    const r = (Math.random() * 16) | 0;
-    // tslint:disable-next-line:no-bitwise
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-};
-
 export const logError = (err?: Error) => {
   if (err) {
     console.error(err.message);
