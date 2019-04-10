b674e053e895fd7d1347431e744166893fe17060
Erik Vullings <erik.vullings@gmail.com>
2019-04-10 18:21:44 +0200

    Added certificates - will be replaced during production

--- packages/server/src/config/kafka.ts
+++ packages/server/src/config/kafka.ts
@@ -9,17 +9,19 @@ import {
 } from 'node-test-bed-adapter';
 
 export default {
-  kafkaHost: process.env.KAFKA_HOST || 'driver-testbed.eu:3541',
-  schemaRegistry: process.env.SCHEMA_REGISTRY || 'driver-testbed.eu:3542',
+  // kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
+  // schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
+  kafkaHost: process.env.KAFKA_HOST || 'driver-testbed.eu:3551',
+  schemaRegistry: process.env.SCHEMA_REGISTRY || 'driver-testbed.eu:3552',
   // kafkaHost: 'driver-testbed.eu:3501',
   // schemaRegistry: 'driver-testbed.eu:3502',
-  // sslOptions: {
-  //   pfx: fs.readFileSync('certs/TB-TrialMgmt.p12'),
-  //   passphrase: 'changeit',
-  //   ca: fs.readFileSync('certs/test-ca.pem'),
-  //   rejectUnauthorized: true,
-  // },
-  clientId: 'TB-TrialMgmt',
+  sslOptions: {
+    pfx: fs.readFileSync('certs/TB-TrialMgmt.p12'),
+    passphrase: 'changeit',
+    ca: fs.readFileSync('certs/test-ca.pem'),
+    rejectUnauthorized: true,
+  },
+  clientId: process.env.CLIENT_ID || 'TB-TrialMgmt',
   fetchAllSchemas: false,
   fetchAllVersions: false,
   // autoRegisterSchemas: true,
@@ -27,16 +29,17 @@ export default {
   wrapUnions: 'auto',
   schemaFolder: './data/schemas',
   // consume: [],
-  produce: [
-    RequestChangeOfTrialStage,
-    TrialManagementPhaseMessageTopic,
-    TrialManagementRolePlayerTopic,
-    TrialManagementSessionMgmtTopic,
-    'system_tm_session_mgmt',
-    'standard_cap',
-    // 'standard_geojson',
-    // 'standard_named_geojson',
-  ],
+  produce: process.env.PRODUCE
+    ? JSON.parse(process.env.PRODUCE)
+    : [
+        RequestChangeOfTrialStage,
+        TrialManagementPhaseMessageTopic,
+        TrialManagementRolePlayerTopic,
+        TrialManagementSessionMgmtTopic,
+        'standard_cap',
+        // 'standard_geojson',
+        // 'standard_named_geojson',
+      ],
   logging: {
     logToConsole: LogLevel.Debug,
     logToKafka: LogLevel.Warn,
