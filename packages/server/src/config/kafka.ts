c8e0c131d56d3e2379f359dc43847a31cf8f7a88
Erik Vullings <erik.vullings@gmail.com>
2019-04-27 09:08:58 +0200

    Enable SSL options

--- packages/server/src/config/kafka.ts
+++ packages/server/src/config/kafka.ts
@@ -9,13 +9,15 @@ import {
 } from 'node-test-bed-adapter';
 
 export default {
+  kafkaHost: process.env.KAFKA_HOST || 'tb4.driver-testbed.eu:3541',
+  schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb4.driver-testbed.eu:3542',
   // kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
   // schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
-  kafkaHost: process.env.KAFKA_HOST || 'tb5.driver-testbed.eu:3551',
-  schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb5.driver-testbed.eu:3552',
+  // kafkaHost: process.env.KAFKA_HOST || 'tb5.driver-testbed.eu:3551',
+  // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb5.driver-testbed.eu:3552',
   // kafkaHost: 'driver-testbed.eu:3501',
   // schemaRegistry: 'driver-testbed.eu:3502',
-  sslOptions: process.env.SSL !== 'false'
+  sslOptions: process.env.SSL === 'true'
     ? {
         pfx: fs.readFileSync(process.env.SSL_PFX || 'certs/TB-TrialMgmt.p12'),
         passphrase: process.env.SSL_PASSPHRASE || 'changeit',
@@ -39,7 +41,7 @@ export default {
         TrialManagementRolePlayerTopic,
         TrialManagementSessionMgmtTopic,
         'standard_cap',
-        // 'standard_geojson',
+        'standard_geojson',
         // 'standard_named_geojson',
       ],
   logging: {
