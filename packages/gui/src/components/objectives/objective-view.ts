3c8f586091cfafb2cb52c8622e80a6c5d06fa77f
Erik Vullings <erik.vullings@gmail.com>
2019-05-08 21:02:05 +0200

    Add a scrollbar to views on mobile

--- packages/gui/src/components/objectives/objective-view.ts
+++ packages/gui/src/components/objectives/objective-view.ts
@@ -4,7 +4,7 @@ import { ObjectiveForm } from './objective-form';
 
 export const ObjectivesView = () => {
   return {
-    view: () => m('.row', [
+    view: () => m('.row.sb.large', [
       m('.col.s12.m5.l4', m(ObjectivesList)),
       m('.col.s12.m7.l8', m(ObjectiveForm)),
     ]),
