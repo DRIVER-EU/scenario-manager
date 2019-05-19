d691bf7aad09438c62450f1a05d8d5617d57be74
Erik Vullings <erik.vullings@gmail.com>
2019-05-19 19:26:47 +0200

    Add tabs to injects view: msg or timeline

--- packages/gui/src/components/injects/injects-view.ts
+++ packages/gui/src/components/injects/injects-view.ts
@@ -2,11 +2,32 @@ import m from 'mithril';
 import { InjectsList } from './injects-list';
 import { InjectsForm } from './injects-form';
 import { InjectsTimeline } from './injects-timeline';
+import { injectsChannel, TopicNames } from '../../models';
 
 export const InjectsView = () => {
+  const state = {
+    msgTabActive: false,
+    subscription: injectsChannel.subscribe(TopicNames.ITEM, ({ cur }) => {
+      state.msgTabActive = true;
+      m.redraw();
+    }),
+  };
+
+  const initTabs = (dom: Element) => {
+    // https://github.com/Dogfalo/materialize/issues/4159 when swipeable is true
+    M.Tabs.init(dom, {
+      onShow: () => (state.msgTabActive = false),
+      swipeable: false,
+    });
+  };
+
   return {
-    view: () =>
-      m('.row.sb.large', [
+    onremove: () => {
+      state.subscription.unsubscribe();
+    },
+    view: () => {
+      const { msgTabActive } = state;
+      return m('.row.sb.large', [
         m('.col.s12.m5.l4', m(InjectsList)),
         m(
           '.col.s12.m7.l8',
@@ -17,19 +38,19 @@ export const InjectsView = () => {
                 'ul.tabs',
                 {
                   style: 'margin-top: 12px;', // to align with the filter
-                  // https://github.com/Dogfalo/materialize/issues/4159 when swipeable is true
-                  oncreate: ({ dom }) => M.Tabs.init(dom, { swipeable: false }),
+                  oncreate: ({ dom }) => initTabs(dom),
                 },
                 [
-                  m('li.tab.col.s3', m('a[href="#timeline"]', 'Timeline')),
-                  m('li.tab.col.s3', m('a[href="#message"]', 'Message')),
+                  m('li.tab.col.s3', m('a[href="#timeline"]', { className: msgTabActive ? '' : 'active' }, 'Timeline')),
+                  m('li.tab.col.s3', m('a[href="#message"]', { className: msgTabActive ? 'active' : '' }, 'Message')),
                 ]
               )
             ),
-            m('.col.s12[id=message]', m(InjectsForm)),
             m('.col.s12[id=timeline]', m(InjectsTimeline)),
+            m('.col.s12[id=message]', m(InjectsForm)),
           ])
         ),
-      ]),
+      ]);
+    },
   };
 };
