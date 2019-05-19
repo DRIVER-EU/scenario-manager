6c28c20ca61e1982ea951fa3eca202ea3400e776
Erik Vullings <erik.vullings@gmail.com>
2019-05-19 19:24:58 +0200

    NEW: Resend message

--- packages/gui/src/components/executing/manual-transition.ts
+++ packages/gui/src/components/executing/manual-transition.ts
@@ -1,5 +1,5 @@
 import m, { FactoryComponent } from 'mithril';
-import { InjectState, InjectConditionType } from 'trial-manager-models';
+import { InjectState, InjectConditionType, InjectType } from 'trial-manager-models';
 import { IExecutingInject } from '../../models';
 import { FlatButton } from 'mithril-materialized';
 import { RunSvc } from '../../services';
@@ -17,20 +17,37 @@ export const ManualTransition: FactoryComponent<{ inject: IExecutingInject }> = 
   return {
     view: ({ attrs: { inject } }) => {
       const { show } = state;
-      return show && waitingForManualConfirmation(inject)
-        ? m(
-            '.row',
-            m(FlatButton, {
-              className: 'right',
-              iconName: 'check_circle',
-              iconClass: 'red-text',
-              label: 'Click here when ready',
-              onclick: () => {
-                state.show = false;
-                RunSvc.transition({ id: inject.id, from: inject.state, to: InjectState.IN_PROGRESS });
-              },
-            })
-          )
+      const isWaiting = waitingForManualConfirmation(inject);
+      return show
+        ? isWaiting
+          ? m(
+              '.row',
+              m(FlatButton, {
+                className: 'right red-text',
+                iconName: 'check_circle',
+                iconClass: 'red-text right',
+                label: 'Click here when ready',
+                onclick: () => {
+                  state.show = false;
+                  RunSvc.transition({ id: inject.id, from: inject.state, to: InjectState.IN_PROGRESS });
+                },
+              })
+            )
+          : inject.type === InjectType.INJECT
+          ? m(
+              '.row',
+              m(FlatButton, {
+                className: 'right',
+                iconName: 'send',
+                iconClass: 'right',
+                label: 'Resend',
+                onclick: () => {
+                  state.show = false;
+                  RunSvc.transition({ id: inject.id, from: inject.state, to: InjectState.IN_PROGRESS });
+                },
+              })
+            )
+          : undefined
         : undefined;
     },
   };
