df3f4f8abedea701937c9ccea89b99672a4f9e34
Erik Vullings <erik.vullings@gmail.com>
2019-05-20 18:11:11 +0200

    Add style so timeline will have scrollbars

--- packages/gui/src/components/session/session-state.ts
+++ packages/gui/src/components/session/session-state.ts
@@ -4,9 +4,9 @@ import { ExecutingInjectView } from '../executing/executing-inject-view';
 
 export const SessionState: FactoryComponent = () => {
   return {
-    view: () => m('.row.sb.large', [
-      m('.col.sb.large.s12.m6.l4', m(SessionTimelineView)),
-      m('.col.sb.large.s12.m6.l8', m(ExecutingInjectView)),
+    view: () => m('.row', [
+      m('.col.s12.m6.sb.large.sb-hor', m(SessionTimelineView)),
+      m('.col.s12.m6.sb.large', m(ExecutingInjectView)),
     ]),
   };
 };
