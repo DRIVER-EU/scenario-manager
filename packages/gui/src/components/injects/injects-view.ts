d22acdb990f76fa90ed2bef80c8cc4f2b421642a
Erik Vullings <erik.vullings@gmail.com>
2019-05-28 23:00:14 +0200

    Improved alignment

--- packages/gui/src/components/injects/injects-view.ts
+++ packages/gui/src/components/injects/injects-view.ts
@@ -3,8 +3,6 @@ import { Tabs } from 'mithril-materialized';
 import { InjectsList } from './injects-list';
 import { InjectsForm } from './injects-form';
 import { InjectsTimeline } from './injects-timeline';
-import { injectsChannel, TopicNames } from '../../models';
-import { ISubscriptionDefinition } from '../../services';
 
 export const InjectsView = () => {
   const state = {
@@ -18,8 +16,7 @@ export const InjectsView = () => {
       return m('.row.sb.large', [
         m('.col.s12.m5.l4', m(InjectsList)),
         m(
-          '.col.s12.m7.l8',
-          { style: 'margin-top: 11px;' },
+          '.col.s12.m7.l8.timeline-message',
           m(Tabs, {
             tabWidth: 'fixed',
             tabs: [
