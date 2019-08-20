3e77eb1903af6dd1d724543113580ffd7943d245
Erik Vullings <erik.vullings@gmail.com>
2019-08-20 21:48:02 +0200

    AssetService should use the actual baseUrl

--- packages/gui/src/services/asset-service.ts
+++ packages/gui/src/services/asset-service.ts
@@ -2,11 +2,15 @@ import { RestService } from './rest-service';
 import { ChannelNames } from '../models/channels';
 import { IAsset } from 'trial-manager-models';
 import { isJSON } from '../utils';
+import { AppState } from '../models';
 
 export class AssetService extends RestService<IAsset> {
-  constructor(trialId: string) {
-    super(`trials/${trialId}/assets`, ChannelNames.ASSETS);
+  constructor(private pTrialId: string) {
+    super(`trials/${pTrialId}/assets`, ChannelNames.ASSETS);
+    this.baseUrl = AppState.apiService() + `trials/${pTrialId}/assets`;
   }
+
+  public get trialId() { return this.pTrialId; }
 
   public async loadMapOverlay(id: number | string) {
     return super.load(id.toString()) as unknown as GeoJSON.FeatureCollection;
