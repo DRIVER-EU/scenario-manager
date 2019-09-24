194bf3282814aa2ae5d776ed26910a174796b374
Erik Vullings <erik.vullings@gmail.com>
2019-09-24 14:11:44 +0200

    Close #43 Save todo

--- packages/gui/src/components/ui/checklist.ts
+++ packages/gui/src/components/ui/checklist.ts
@@ -1,9 +1,9 @@
 import m, { FactoryComponent } from 'mithril';
 import { IScenario, ITodo } from 'trial-manager-models';
-import { Kanban, IModelField, IKanban } from 'mithril-materialized';
+import { FlatButton, Kanban, IModelField, IKanban } from 'mithril-materialized';
 
 export const Checklist: FactoryComponent<{ scenario: IScenario; disabled?: boolean; onChange?: () => void }> = () => {
-  const state = {} as { onChange?: () => void };
+  const state = { key: 0 } as { key: number; onChange?: () => void };
 
   const model = [
     {
@@ -51,32 +51,60 @@ export const Checklist: FactoryComponent<{ scenario: IScenario; disabled?: boole
       state.onChange = onChange;
     },
     view: ({ attrs: { scenario, disabled = false } }) => {
-      return m('.row', [
-        m('.col.s6', [
-          disabled ? m('h6', 'BEFORE') : undefined,
-          m(Kanban, {
-            disabled,
-            label: 'todo (BEFORE)',
-            canDrag: true,
-            model: disabled ? disabledModel : model,
-            onchange: onchange(scenario, 'before'),
-            items: scenario.todoBefore,
-            editableIds: disabled ? ['done'] : undefined,
-          } as IKanban<ITodo>),
-        ]),
-        m('.col.s6', [
-          disabled ? m('h6', 'AFTER') : undefined,
-          m(Kanban, {
-            disabled,
-            label: 'todo (AFTER)',
-            canDrag: true,
-            model: disabled ? disabledModel : model,
-            onchange: onchange(scenario, 'after'),
-            items: scenario.todoAfter,
-            editableIds: disabled ? ['done'] : undefined,
-          } as IKanban<ITodo>),
-        ]),
-      ]);
+      const { key } = state;
+      const { todoBefore, todoAfter } = scenario;
+      return disabled === false || todoBefore || todoAfter
+        ? m('.row', [
+            m('.col.s6', [
+              disabled ? m('h6', 'BEFORE') : undefined,
+              [
+                m(Kanban, {
+                  key: `before${key}`,
+                  disabled,
+                  label: 'todo (BEFORE)',
+                  canDrag: true,
+                  model: disabled ? disabledModel : model,
+                  onchange: onchange(scenario, 'before'),
+                  items: todoBefore,
+                  editableIds: disabled ? ['done'] : undefined,
+                } as IKanban<ITodo>),
+              ],
+            ]),
+            m('.col.s6', [
+              disabled ? m('h6', 'AFTER') : undefined,
+              [
+                m(Kanban, {
+                  key: `after${key}`,
+                  disabled,
+                  label: 'todo (AFTER)',
+                  canDrag: true,
+                  model: disabled ? disabledModel : model,
+                  onchange: onchange(scenario, 'after'),
+                  items: todoAfter,
+                  editableIds: disabled ? ['done'] : undefined,
+                } as IKanban<ITodo>),
+              ],
+            ]),
+            m(
+              '.col.s12',
+              m(FlatButton, {
+                label: 'Clear all done',
+                iconName: 'clear_all',
+                onclick: () => {
+                  if (todoBefore) {
+                    scenario.todoBefore = todoBefore.map(todo => ({ ...todo, done: false }));
+                    onchange(scenario, 'before');
+                  }
+                  if (todoAfter) {
+                    scenario.todoAfter = todoAfter.map(todo => ({ ...todo, done: false }));
+                    onchange(scenario, 'after');
+                  }
+                  state.key++;
+                },
+              })
+            ),
+          ])
+        : undefined;
     },
   };
 };
