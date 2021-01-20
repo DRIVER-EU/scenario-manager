5dcc5c6ea942ec7b7114688e2a5c7aae12482201
Hein Kluiver <hein.kluiver@tno.nl>
2021-01-20 17:38:52 +0100

    Upgrade to @nestjs/config

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,17 +1,22 @@
 import * as path from 'path';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from 'nestjs-config';
+import { ConfigModule } from '@nestjs/config';
 import { TrialModule } from './api/trials/trial.module';
 import { RepoModule } from './api/repo/repo.module';
 import { TimeEventsModule } from './api/time';
 import { KafkaModule } from './adapters/kafka';
 import { RunModule } from './api/run/run.module';
+import configuration from './config/kafka';
+
 
 // console.log('DIR: ' + __dirname);
 
 @Module({
   imports: [
-    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    ConfigModule.forRoot({
+      /// load: [ path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}') ] }),
+      load: [configuration],
+    }),
     RunModule,
     KafkaModule,
     TrialModule,
@@ -19,4 +24,4 @@ import { RunModule } from './api/run/run.module';
     TimeEventsModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- packages/server/src/config/kafka.ts
+++ packages/server/src/config/kafka.ts
@@ -8,37 +8,38 @@ import {
   TrialManagementSessionMgmtTopic,
 } from 'node-test-bed-adapter';
 
-export default {
-  kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
-  schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
-  // kafkaHost: process.env.KAFKA_HOST || 'tb3.driver-testbed.eu:3531',
-  // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb3.driver-testbed.eu:3532',
-  // kafkaHost: process.env.KAFKA_HOST || 'tb4.driver-testbed.eu:3541',
-  // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb4.driver-testbed.eu:3542',
-  // kafkaHost: process.env.KAFKA_HOST || 'tb5.driver-testbed.eu:3551',
-  // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb5.driver-testbed.eu:3552',
-  // kafkaHost: 'driver-testbed.eu:3501',
-  // schemaRegistry: 'driver-testbed.eu:3502',
-  // sslOptions: true // process.env.SSL === 'true'
-  sslOptions: process.env.SSL === 'true'
-    ? {
+export default () => ({
+  kafka: {
+    kafkaHost: process.env.KAFKA_HOST || 'localhost:3501',
+    schemaRegistry: process.env.SCHEMA_REGISTRY || 'localhost:3502',
+    // kafkaHost: process.env.KAFKA_HOST || 'tb3.driver-testbed.eu:3531',
+    // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb3.driver-testbed.eu:3532',
+    // kafkaHost: process.env.KAFKA_HOST || 'tb4.driver-testbed.eu:3541',
+    // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb4.driver-testbed.eu:3542',
+    // kafkaHost: process.env.KAFKA_HOST || 'tb5.driver-testbed.eu:3551',
+    // schemaRegistry: process.env.SCHEMA_REGISTRY || 'tb5.driver-testbed.eu:3552',
+    // kafkaHost: 'driver-testbed.eu:3501',
+    // schemaRegistry: 'driver-testbed.eu:3502',
+    // sslOptions: true // process.env.SSL === 'true'
+    sslOptions: process.env.SSL === 'true'
+      ? {
         pfx: fs.readFileSync(process.env.SSL_PFX || 'certs/TB-TrialMgmt.p12'),
         passphrase: process.env.SSL_PASSPHRASE || 'changeit',
         ca: fs.readFileSync(process.env.SSL_CA || 'certs/test-ca.pem'),
         rejectUnauthorized: true,
       }
-    : undefined,
-  clientId: process.env.CLIENT_ID || 'TB-TrialMgmt',
-  fetchAllSchemas: false,
-  fetchAllVersions: false,
-  // autoRegisterSchemas: true,
-  autoRegisterSchemas: false,
-  wrapUnions: 'auto',
-  schemaFolder: './data/schemas',
-  // consume: [],
-  produce: process.env.PRODUCE
-    ? process.env.PRODUCE.split(',')
-    : [
+      : undefined,
+    clientId: process.env.CLIENT_ID || 'TB-TrialMgmt',
+    fetchAllSchemas: false,
+    fetchAllVersions: false,
+    // autoRegisterSchemas: true,
+    autoRegisterSchemas: false,
+    wrapUnions: 'auto',
+    schemaFolder: './data/schemas',
+    // consume: [],
+    produce: process.env.PRODUCE
+      ? process.env.PRODUCE.split(',')
+      : [
         RequestChangeOfTrialStage,
         TrialManagementPhaseMessageTopic,
         TrialManagementRolePlayerTopic,
@@ -54,8 +55,9 @@ export default {
         // 'simulation_request_unittransport',
         // 'standard_named_geojson',
       ],
-  logging: {
-    logToConsole: LogLevel.Debug,
-    logToKafka: LogLevel.Warn,
-  },
-} as ITestBedOptions;
+    logging: {
+      logToConsole: LogLevel.Debug,
+      logToKafka: LogLevel.Warn,
+    },
+  } as ITestBedOptions,
+});
